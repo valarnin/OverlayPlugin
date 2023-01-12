437e694294591000feac676e79a18375e046ab23
valarnin <valarnin@gmail.com>
2023-01-11 23:21:00 -0500

    Bump aers/FFXIVClientStructs to latest Rework FFXIVClientStructs prep logic to use more concrete logic

--- DEPS.py
+++ DEPS.py
@@ -22,9 +22,9 @@ deps = {
         'hash': ['sha256', '032f43f2674008c761af19bf536374128c16241fb234699a55f9fb603fcfbae7'],
     },
     'FFXIVClientStructs': {
-        'url': 'https://github.com/aers/FFXIVClientStructs/archive/9cb2eb33826f62f7e3525e1f6ca08a974b35c76b.zip',
+        'url': 'https://github.com/aers/FFXIVClientStructs/archive/300b9c5401ebf67015517ba4a38c25b4a993bb7f.zip',
         'dest': 'OverlayPlugin.Core/Thirdparty/FFXIVClientStructs/Global',
         'strip': 1,
-        'hash': ['sha256', '8c9307a01a479f6253ad205fffe8a711302d5c6418fded4b7044919f8ce42269'],
+        'hash': ['sha256', '38c25a7cd3fba30fdc1f596a1f096e159d141385f197a89a54d3cbb76a8c466b'],
     },
 }
