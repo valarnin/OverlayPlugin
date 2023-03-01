025214bb19710bbaf62d19181c09a461d587a4ad
valarnin <valarnin@gmail.com>
2023-03-01 14:34:22 -0500

    Bump FFXIVClientStructs to latest Clean up some old files related to FFXIVClientStructs Rewrite ManagedType to: 1. reduce code 2. reduce stack overhead 3. improve performance 4. improve output Add more addon mappings to AtkStageMemory62

--- DEPS.py
+++ DEPS.py
@@ -22,9 +22,9 @@ deps = {
         'hash': ['sha256', '032f43f2674008c761af19bf536374128c16241fb234699a55f9fb603fcfbae7'],
     },
     'FFXIVClientStructs': {
-        'url': 'https://github.com/aers/FFXIVClientStructs/archive/218cea8679cf8e9630440dfd6d7cf3294c9e0280.zip',
+        'url': 'https://github.com/aers/FFXIVClientStructs/archive/8d454fb6209b994c47d1338f7cfdb8d0cd64ee52.zip',
         'dest': 'OverlayPlugin.Core/Thirdparty/FFXIVClientStructs/Base/Global',
         'strip': 1,
-        'hash': ['sha256', 'c790fd49c05991db2c83f36d6493fc8d73491c6e682182258584e7841163258f'],
+        'hash': ['sha256', 'db7b3e1bc85769229b02d2e27dbae75a2bc84cfc43e6b934de95570f74ab958e'],
     },
 }
