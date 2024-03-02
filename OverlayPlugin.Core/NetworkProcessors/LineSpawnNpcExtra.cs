3e8d64101cf1a0d02d7691209339e2fa68bebc2b
valarnin <valarnin@gmail.com>
2024-03-02 16:01:58 -0500

    Clean up `using`s Add `animationState` and info

--- OverlayPlugin.Core/NetworkProcessors/LineSpawnNpcExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineSpawnNpcExtra.cs
@@ -1,10 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
+
+/*
+At initial zone-in of Barbariccia normal, there are 9 actors that spawn with `animationState` of `01`:
+272|2024-03-02T15:45:44.2260000-05:00|4000226B|E0000000|0000|01|89d2d9b95839548f
+272|2024-03-02T15:45:44.2260000-05:00|4000226D|E0000000|0000|01|b5e6a59cc0b2c1f3
+272|2024-03-02T15:45:44.2260000-05:00|4000226E|E0000000|0000|01|0864edbcb74f9721
+272|2024-03-02T15:45:44.2260000-05:00|4000226F|E0000000|0000|01|56b1f6fddd97dede
+272|2024-03-02T15:45:44.2260000-05:00|40002270|E0000000|0000|01|036aae0ca3f7ebda
+272|2024-03-02T15:45:44.2260000-05:00|40002271|E0000000|0000|01|1396bfae913f5832
+272|2024-03-02T15:45:44.2260000-05:00|4000226C|E0000000|0000|01|a11c9d39fdbeeb7a
+272|2024-03-02T15:45:44.2260000-05:00|4000226A|E0000000|0000|01|0fef26e4fb626dda
+272|2024-03-02T15:45:44.2700000-05:00|40002272|E0000000|0000|01|55ca54ac2707553a
+03|2024-03-02T15:45:44.4890000-05:00|4000226C|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|898c4e2990d59eec
+03|2024-03-02T15:45:44.4890000-05:00|40002271|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|b0a23f029b8f2da5
+03|2024-03-02T15:45:44.4890000-05:00|40002270|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|c46da1ddfd7e3ed2
+03|2024-03-02T15:45:44.4890000-05:00|4000226A|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|723a0d74b51582c2
+03|2024-03-02T15:45:44.4890000-05:00|40002272|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|890e57d3f5d9809d
+03|2024-03-02T15:45:44.4890000-05:00|4000226B|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|16e2d766f065a0fd
+03|2024-03-02T15:45:44.4890000-05:00|4000226F|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|0391ea7f37b7098f
+03|2024-03-02T15:45:44.4890000-05:00|4000226E|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|df9cee50012d0b5d
+03|2024-03-02T15:45:44.4890000-05:00|4000226D|Barbariccia|00|5A|0000|00||11398|14802|69200|69200|10000|10000|||100.00|100.00|0.00|0.00|70bf7f0124bc77c7
+ */
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
 {
@@ -25,6 +45,9 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
 
             [FieldOffset(0x9A)]
             public ushort tetherId;
+
+            [FieldOffset(0xB3)]
+            public byte animationState;
         }
 
         private class RegionalizedInfo
@@ -100,10 +123,11 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
                     var packet = Marshal.PtrToStructure<Server_NpcSpawn>(new IntPtr(buffer));
                     var parentActorId = packet.parentActorId;
                     var tetherId = packet.tetherId;
+                    var animationState = packet.animationState;
 
                     string line = string.Format(CultureInfo.InvariantCulture,
-                        "{0:X8}|{1:X8}|{2:X4}",
-                        sourceId, parentActorId, tetherId);
+                        "{0:X8}|{1:X8}|{2:X4}|{3:X2}",
+                        sourceId, parentActorId, tetherId, animationState);
 
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
                     logWriter(line, serverTime);
