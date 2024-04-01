5e35341f0e8a952c06d4f5bb9be26ab690e67d3a
valarnin <valarnin@gmail.com>
2024-04-01 19:17:03 -0400

    Update comments on `LineActorControlSelfExtra` to properly indicate how to test

--- OverlayPlugin.Core/NetworkProcessors/LineActorControlSelfExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineActorControlSelfExtra.cs
@@ -5,11 +5,22 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// The easiest place to test SetAnimationState is Lunar Subteranne.
-// On the first Ruinous Confluence, each staff has this line:
-// 273|2023-12-05T10:57:43.4770000-08:00|4000A145|003E|00000001|00000000|00000000|00000000|06e7eff4a949812c
-// On the second Ruinous Confluence, each staff has this line:
-// 273|2023-12-05T10:58:00.3460000-08:00|4000A144|003E|00000001|00000001|00000000|00000000|a4af9f90928636a3
+// To test `DisplayLogMessage`, you can:
+// Open a treasure coffer that gives you item(s):
+// 274|2024-01-10T19:28:37.5000000-05:00|10001234|020F|04D0|0|93E0|0|0|0|d274429622d0c27e
+// 274|2024-01-10T19:28:37.5000000-05:00|10001234|020F|04D0|0|93F3|0|0|0|d274429622d0c27e
+// 00|2024-01-10T19:28:36.0000000-05:00|0A3E||You obtain a windswept shamshir.|92337ce2a33e52f8
+// 00|2024-01-10T19:28:36.0000000-05:00|0A3E||You obtain a windswept shield.|a48cbf20d0255d4e
+// Sell TT cards for MGP:
+// 274|2024-01-10T20:08:35.3520000-05:00|10001234|0210|129D|0|320|0|0|0|d274429622d0c27e
+// 00|2024-01-10T20:08:35.0000000-05:00|0A3E||You obtain 800 MGP.|f768dc4f098c15a6
+// Die in Eureka with a `Spirit of the Remembered` active:
+// 274|2024-02-15T19:35:41.9950000-05:00|10001234|020F|236D|0|669|0|0|0|d274429622d0c27e
+// 00|2024-02-15T19:35:41.0000000-05:00|0939||The memories of heroes past live on again!|bb3bfbfc487ad4e9
+
+// To test `DisplayLogMessageParams`, you can play a Gold Saucer minigame:
+// 274|2024-03-21T20:45:41.3680000-04:00|10001234|0210|129D|10001234|F|0|0|0|d274429622d0c27e
+// 00|2024-03-21T20:45:40.0000000-04:00|08BE||You obtain 15 MGP.|97702e809544a633
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
 {
@@ -21,6 +32,8 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
 
         // Any category defined in this array will be allowed as an emitted line
         public static readonly Server_ActorControlCategory[] AllowedActorControlCategories = {
+            // Some `LogMessage` messages can be triggered by both 0x020F and 0x0210 categories, not sure what the difference is
+            // except that 0x0210 messages usually have another actor ID in the parameters
             Server_ActorControlCategory.DisplayLogMessage,
             Server_ActorControlCategory.DisplayLogMessageParams,
         };
