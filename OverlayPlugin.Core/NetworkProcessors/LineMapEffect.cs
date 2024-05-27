d44eb5f11103d059ce22b8c36ae3ef7afff535d7
valarnin <valarnin@gmail.com>
2024-05-27 15:24:39 -0400

    Convert LineMapEffect

--- OverlayPlugin.Core/NetworkProcessors/LineMapEffect.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineMapEffect.cs
@@ -1,66 +1,62 @@
 ﻿using System;
-using System.Reflection;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using RainbowMage.OverlayPlugin.NetworkProcessors.PacketHelper;
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
 {
-    [Serializable]
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct MapEffect_v62
-    {
-        [FieldOffset(0x0)]
-        public uint instanceContentID;
-        [FieldOffset(0x4)]
-        public uint flags;
-        [FieldOffset(0x8)]
-        public byte index;
-        [FieldOffset(0x9)]
-        public byte unknown1;
-        [FieldOffset(0x10)]
-        public ushort unknown2;
-
-        public override string ToString()
-        {
-            return $"{instanceContentID:X8}|{flags:X8}|{index:X2}|{unknown1:X2}|{unknown2:X4}";
-        }
-    }
+    using RPH = RegionalizedPacketHelper<
+            Server_MessageHeader_Global, LineActorMove.ActorMove_v655,
+            Server_MessageHeader_CN, LineActorMove.ActorMove_v655,
+            Server_MessageHeader_KR, LineActorMove.ActorMove_v655>;
 
     public class LineMapEffect
     {
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MapEffect_v62 : IPacketStruct
+        {
+            [FieldOffset(0x0)]
+            public uint instanceContentID;
+            [FieldOffset(0x4)]
+            public uint flags;
+            [FieldOffset(0x8)]
+            public byte index;
+            [FieldOffset(0x9)]
+            public byte unknown1;
+            [FieldOffset(0x10)]
+            public ushort unknown2;
+
+            public string ToString(long epoch, uint ActorID)
+            {
+                return $"{instanceContentID:X8}|{flags:X8}|{index:X2}|{unknown1:X2}|{unknown2:X4}";
+            }
+        }
+
         public const uint LogFileLineID = 257;
-        private ILogger logger;
-        private OverlayPluginLogLineConfig opcodeConfig;
-        private IOpcodeConfigEntry opcode = null;
-        private readonly int offsetMessageType;
-        private readonly int offsetPacketData;
+
         private readonly FFXIVRepository ffxiv;
 
         private Func<string, DateTime, bool> logWriter;
+        private RPH packetHelper;
+        private GameRegion? currentRegion;
 
         public LineMapEffect(TinyIoCContainer container)
         {
-            logger = container.Resolve<ILogger>();
             ffxiv = container.Resolve<FFXIVRepository>();
-            var netHelper = container.Resolve<NetworkParser>();
-            if (!ffxiv.IsFFXIVPluginPresent())
+            ffxiv.RegisterNetworkParser(MessageReceived);
+            ffxiv.RegisterProcessChangedHandler(ProcessChanged);
+
+            var opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
+
+            packetHelper = RPH.CreateFromOpcodeConfig(opcodeConfig, "MapEffect");
+
+            if (packetHelper == null)
+            {
+                var logger = container.Resolve<ILogger>();
+                logger.Log(LogLevel.Error, "Failed to initialize LineMapEffect: Failed to create MapEffect packet helper from opcode configs and native structs");
                 return;
-            opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
-            try
-            {
-                var mach = Assembly.Load("Machina.FFXIV");
-                var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                offsetMessageType = netHelper.GetOffset(msgHeaderType, "MessageType");
-                offsetPacketData = Marshal.SizeOf(msgHeaderType);
-                ffxiv.RegisterNetworkParser(MessageReceived);
             }
-            catch (System.IO.FileNotFoundException)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserNoFfxiv);
-            }
-            catch (Exception e)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
-            }
+
             var customLogLines = container.Resolve<FFXIVCustomLogLines>();
             this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
             {
@@ -71,33 +67,31 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             });
         }
 
-        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        private void ProcessChanged(Process process)
         {
-            // Wait for network data to actually fetch opcode info from file and register log line
-            // This is because the FFXIV_ACT_Plugin's `GetGameVersion` method only returns valid data
-            // if the player is currently logged in/a network connection is active
-            if (opcode == null)
-            {
-                opcode = opcodeConfig["MapEffect"];
-                if (opcode == null)
-                {
-                    return;
-                }
-            }
-
-            if (message.Length < opcode.size + offsetPacketData)
+            if (!ffxiv.IsFFXIVPluginPresent())
                 return;
 
-            fixed (byte* buffer = message)
-            {
-                if (*(ushort*)&buffer[offsetMessageType] == opcode.opcode)
-                {
-                    DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                    MapEffect_v62 mapEffectPacket = *(MapEffect_v62*)&buffer[offsetPacketData];
-                    logWriter(mapEffectPacket.ToString(), serverTime);
+            currentRegion = null;
+        }
 
-                    return;
-                }
+        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        {
+            if (packetHelper == null)
+                return;
+
+            if (currentRegion == null)
+                currentRegion = ffxiv.GetMachinaRegion();
+
+            if (currentRegion == null)
+                return;
+
+            var line = packetHelper[currentRegion.Value].ToString(epoch, message);
+
+            if (line != null)
+            {
+                DateTime serverTime = ffxiv.EpochToDateTime(epoch);
+                logWriter(line, serverTime);
             }
         }
 
