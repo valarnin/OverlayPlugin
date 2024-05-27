aae2efd058c7522bab1f10ea21754e5ec975e99d
valarnin <valarnin@gmail.com>
2024-05-27 15:39:51 -0400

    Convert LineRSV, also fix struct to match new knowledge

--- OverlayPlugin.Core/NetworkProcessors/LineRSV.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineRSV.cs
@@ -1,84 +1,69 @@
 ﻿using System;
-using System.Reflection;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using RainbowMage.OverlayPlugin.MemoryProcessors;
+using RainbowMage.OverlayPlugin.NetworkProcessors.PacketHelper;
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
 {
-    [StructLayout(LayoutKind.Explicit, Size = structSize, Pack = 1)]
-    internal unsafe struct RSV_v62
-    {
-        public const int structSize = 1080;
-        public const int keySize = 0x30;
-        public const int valueSize = 0x404;
-        [FieldOffset(0x0)]
-        public uint unknown1;
-        [FieldOffset(0x4)]
-        public fixed byte key[keySize];
-        [FieldOffset(0x34)]
-        public fixed byte value[valueSize];
-
-        public override string ToString()
-        {
-            fixed (byte* key = this.key) fixed (byte* value = this.value)
-            {
-                return
-                    $"|" +
-                    $"{unknown1:X8}|" +
-                    $"{FFXIVMemory.GetStringFromBytes(key, keySize).Replace("\r", "\\r").Replace("\n", "\\n")}|" +
-                    $"{FFXIVMemory.GetStringFromBytes(value, valueSize).Replace("\r", "\\r").Replace("\n", "\\n")}";
-            }
-        }
-
-        public string ToString(string locale)
-        {
-            fixed (byte* key = this.key) fixed (byte* value = this.value)
-            {
-                return
-                    $"{locale}|" +
-                    $"{unknown1:X8}|" +
-                    $"{FFXIVMemory.GetStringFromBytes(key, keySize).Replace("\r", "\\r").Replace("\n", "\\n")}|" +
-                    $"{FFXIVMemory.GetStringFromBytes(value, valueSize).Replace("\r", "\\r").Replace("\n", "\\n")}";
-            }
-        }
-    }
+    using RPH = RegionalizedPacketHelper<
+            Server_MessageHeader_Global, LineRSV.RSV_v62,
+            Server_MessageHeader_CN, LineRSV.RSV_v62,
+            Server_MessageHeader_KR, LineRSV.RSV_v62>;
 
     public class LineRSV
     {
+        [StructLayout(LayoutKind.Explicit, Size = structSize, Pack = 1)]
+        internal unsafe struct RSV_v62 : IPacketStruct
+        {
+            public const int structSize = 1080;
+            public const int keySize = 0x30;
+            public const int valueSize = 0x404;
+            [FieldOffset(0x0)]
+            public int valueByteCount;
+            [FieldOffset(0x4)]
+            public fixed byte key[keySize];
+            [FieldOffset(0x34)]
+            public fixed byte value[valueSize];
+
+            public string ToString(long epoch, uint ActorID)
+            {
+                fixed (byte* key = this.key) fixed (byte* value = this.value)
+                {
+                    return
+                        $"{ffxiv.GetLocaleString()}|" +
+                        $"{valueByteCount:X8}|" +
+                        $"{FFXIVMemory.GetStringFromBytes(key, keySize).Replace("\r", "\\r").Replace("\n", "\\n")}|" +
+                        $"{FFXIVMemory.GetStringFromBytes(value, Math.Min(valueByteCount, valueSize)).Replace("\r", "\\r").Replace("\n", "\\n")}";
+                }
+            }
+        }
+
         public const uint LogFileLineID = 262;
-        private ILogger logger;
-        private OverlayPluginLogLineConfig opcodeConfig;
-        private IOpcodeConfigEntry opcode = null;
-        private readonly int offsetMessageType;
-        private readonly int offsetPacketData;
-        private readonly FFXIVRepository ffxiv;
+
+        private static FFXIVRepository ffxiv;
 
         private Func<string, DateTime, bool> logWriter;
+        private RPH packetHelper;
+        private GameRegion? currentRegion;
 
         public LineRSV(TinyIoCContainer container)
         {
-            logger = container.Resolve<ILogger>();
             ffxiv = container.Resolve<FFXIVRepository>();
-            var netHelper = container.Resolve<NetworkParser>();
-            if (!ffxiv.IsFFXIVPluginPresent())
+            ffxiv.RegisterNetworkParser(MessageReceived);
+            ffxiv.RegisterProcessChangedHandler(ProcessChanged);
+
+            var opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
+
+            packetHelper = RPH.CreateFromOpcodeConfig(opcodeConfig, "ActorMove");
+
+            if (packetHelper == null)
+            {
+                var logger = container.Resolve<ILogger>();
+                logger.Log(LogLevel.Error, "Failed to initialize LineActorMove: Failed to create ActorMove packet helper from opcode configs and native structs");
                 return;
-            opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
-            try
-            {
-                var mach = Assembly.Load("Machina.FFXIV");
-                var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                offsetMessageType = netHelper.GetOffset(msgHeaderType, "MessageType");
-                offsetPacketData = Marshal.SizeOf(msgHeaderType);
-                ffxiv.RegisterNetworkParser(MessageReceived);
             }
-            catch (System.IO.FileNotFoundException)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserNoFfxiv);
-            }
-            catch (Exception e)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
-            }
+
             var customLogLines = container.Resolve<FFXIVCustomLogLines>();
             this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
             {
@@ -89,33 +74,31 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             });
         }
 
-        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        private void ProcessChanged(Process process)
         {
-            // Wait for network data to actually fetch opcode info from file and register log line
-            // This is because the FFXIV_ACT_Plugin's `GetGameVersion` method only returns valid data
-            // if the player is currently logged in/a network connection is active
-            if (opcode == null)
-            {
-                opcode = opcodeConfig["RSVData"];
-                if (opcode == null)
-                {
-                    return;
-                }
-            }
-
-            if (message.Length < opcode.size + offsetPacketData)
+            if (!ffxiv.IsFFXIVPluginPresent())
                 return;
 
-            fixed (byte* buffer = message)
-            {
-                if (*(ushort*)&buffer[offsetMessageType] == opcode.opcode)
-                {
-                    DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                    RSV_v62 RSVPacket = *(RSV_v62*)&buffer[offsetPacketData];
-                    logWriter(RSVPacket.ToString(ffxiv.GetLocaleString()), serverTime);
+            currentRegion = null;
+        }
 
-                    return;
-                }
+        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        {
+            if (packetHelper == null)
+                return;
+
+            if (currentRegion == null)
+                currentRegion = ffxiv.GetMachinaRegion();
+
+            if (currentRegion == null)
+                return;
+
+            var line = packetHelper[currentRegion.Value].ToString(epoch, message);
+
+            if (line != null)
+            {
+                DateTime serverTime = ffxiv.EpochToDateTime(epoch);
+                logWriter(line, serverTime);
             }
         }
     }
