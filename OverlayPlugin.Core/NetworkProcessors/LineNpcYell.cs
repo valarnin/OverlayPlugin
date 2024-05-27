f5a1a9b2796204a54c839d3f6662cfee60bd7455
valarnin <valarnin@gmail.com>
2024-05-27 15:27:08 -0400

    Convert LineNpcYell

--- OverlayPlugin.Core/NetworkProcessors/LineNpcYell.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineNpcYell.cs
@@ -1,73 +1,69 @@
 ﻿using System;
-using System.Reflection;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
-using RainbowMage.OverlayPlugin.MemoryProcessors;
+using RainbowMage.OverlayPlugin.NetworkProcessors.PacketHelper;
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
 {
-    [StructLayout(LayoutKind.Explicit, Size = structSize, Pack = 1)]
-    internal unsafe struct NpcYell_v655
-    {
-        // 00|2024-02-22T22:35:03.0000000-05:00|0044|Shanoa|Meow!♪|1d173e4a0eacfd95
-        // 6.5.5 packet data (minus header):
-        // 8A6B0140 00000000 0624 0000 D624 00000000 00000000 00000000 00000000 0000
-        // AAAAAAAA BBBBBBBB CCCC DDDD EEEE FFFFFFFF GGGGGGGG HHHHHHHH IIIIIIII JJJJ
-        // 0x0      0x4      0x8  0xA  0xC
-        // Actor ID          NameID    YellID
-
-        public const int structSize = 32;
-        [FieldOffset(0x0)]
-        public uint actorID;
-        [FieldOffset(0x8)]
-        public ushort nameID;
-        [FieldOffset(0xC)]
-        public ushort yellID;
-
-        public override string ToString()
-        {
-            return
-                $"{actorID:X8}|" +
-                $"{nameID:X4}|" +
-                $"{yellID:X4}";
-        }
-    }
+    using RPH = RegionalizedPacketHelper<
+            Server_MessageHeader_Global, LineActorMove.ActorMove_v655,
+            Server_MessageHeader_CN, LineActorMove.ActorMove_v655,
+            Server_MessageHeader_KR, LineActorMove.ActorMove_v655>;
 
     public class LineNpcYell
     {
+        [StructLayout(LayoutKind.Explicit, Size = structSize, Pack = 1)]
+        internal unsafe struct NpcYell_v655 : IPacketStruct
+        {
+            // 00|2024-02-22T22:35:03.0000000-05:00|0044|Shanoa|Meow!♪|1d173e4a0eacfd95
+            // 6.5.5 packet data (minus header):
+            // 8A6B0140 00000000 0624 0000 D624 00000000 00000000 00000000 00000000 0000
+            // AAAAAAAA BBBBBBBB CCCC DDDD EEEE FFFFFFFF GGGGGGGG HHHHHHHH IIIIIIII JJJJ
+            // 0x0      0x4      0x8  0xA  0xC
+            // Actor ID          NameID    YellID
+
+            public const int structSize = 32;
+            [FieldOffset(0x0)]
+            public uint actorID;
+            [FieldOffset(0x8)]
+            public ushort nameID;
+            [FieldOffset(0xC)]
+            public ushort yellID;
+
+            public string ToString(long epoch, uint ActorID)
+            {
+                return
+                    $"{actorID:X8}|" +
+                    $"{nameID:X4}|" +
+                    $"{yellID:X4}";
+            }
+        }
+
         public const uint LogFileLineID = 266;
-        private ILogger logger;
-        private OverlayPluginLogLineConfig opcodeConfig;
-        private IOpcodeConfigEntry opcode = null;
-        private readonly int offsetMessageType;
-        private readonly int offsetPacketData;
+
         private readonly FFXIVRepository ffxiv;
 
         private Func<string, DateTime, bool> logWriter;
+        private RPH packetHelper;
+        private GameRegion? currentRegion;
 
         public LineNpcYell(TinyIoCContainer container)
         {
-            logger = container.Resolve<ILogger>();
             ffxiv = container.Resolve<FFXIVRepository>();
-            var netHelper = container.Resolve<NetworkParser>();
-            if (!ffxiv.IsFFXIVPluginPresent())
+            ffxiv.RegisterNetworkParser(MessageReceived);
+            ffxiv.RegisterProcessChangedHandler(ProcessChanged);
+
+            var opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
+
+            packetHelper = RPH.CreateFromOpcodeConfig(opcodeConfig, "NpcYell");
+
+            if (packetHelper == null)
+            {
+                var logger = container.Resolve<ILogger>();
+                logger.Log(LogLevel.Error, "Failed to initialize LineNpcYell: Failed to create NpcYell packet helper from opcode configs and native structs");
                 return;
-            opcodeConfig = container.Resolve<OverlayPluginLogLineConfig>();
-            try
-            {
-                var mach = Assembly.Load("Machina.FFXIV");
-                var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                offsetMessageType = netHelper.GetOffset(msgHeaderType, "MessageType");
-                offsetPacketData = Marshal.SizeOf(msgHeaderType);
-                ffxiv.RegisterNetworkParser(MessageReceived);
             }
-            catch (System.IO.FileNotFoundException)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserNoFfxiv);
-            }
-            catch (Exception e)
-            {
-                logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
-            }
+
             var customLogLines = container.Resolve<FFXIVCustomLogLines>();
             this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
             {
@@ -78,33 +74,31 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             });
         }
 
-        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        private void ProcessChanged(Process process)
         {
-            // Wait for network data to actually fetch opcode info from file and register log line
-            // This is because the FFXIV_ACT_Plugin's `GetGameVersion` method only returns valid data
-            // if the player is currently logged in/a network connection is active
-            if (opcode == null)
-            {
-                opcode = opcodeConfig["NpcYell"];
-                if (opcode == null)
-                {
-                    return;
-                }
-            }
-
-            if (message.Length < opcode.size + offsetPacketData)
+            if (!ffxiv.IsFFXIVPluginPresent())
                 return;
 
-            fixed (byte* buffer = message)
-            {
-                if (*(ushort*)&buffer[offsetMessageType] == opcode.opcode)
-                {
-                    DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                    NpcYell_v655 yellPacket = *(NpcYell_v655*)&buffer[offsetPacketData];
-                    logWriter(yellPacket.ToString(), serverTime);
+            currentRegion = null;
+        }
 
-                    return;
-                }
+        private unsafe void MessageReceived(string id, long epoch, byte[] message)
+        {
+            if (packetHelper == null)
+                return;
+
+            if (currentRegion == null)
+                currentRegion = ffxiv.GetMachinaRegion();
+
+            if (currentRegion == null)
+                return;
+
+            var line = packetHelper[currentRegion.Value].ToString(epoch, message);
+
+            if (line != null)
+            {
+                DateTime serverTime = ffxiv.EpochToDateTime(epoch);
+                logWriter(line, serverTime);
             }
         }
     }
